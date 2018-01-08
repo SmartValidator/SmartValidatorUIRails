f76715aa8bb9686bf98282fad639152149a067f0
Andreas Marc Klingler <post@andreas-klingler.de>
2018-01-08 22:59:40 +0100

    Rename timeline into singular

--- app/models/smart_validator_db/timeline_conflict.rb
+++ app/models/smart_validator_db/timeline_conflict.rb
@@ -1,5 +1,5 @@
 # Interface class for a table in the main smart validator database.
-class SmartValidatorDb::TimelineConflicts < ActiveRecord::Base
+class SmartValidatorDb::TimelineConflict < ActiveRecord::Base
   establish_connection(:smart_validator_db)
   self.table_name = 'timeline_conflicts'
 
